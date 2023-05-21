a8f6927c77d86c2ce45d2a2483e835c45ba591cf
Om Sanan <om.sanan007@gmail.com>
2023-05-21 01:20:36 -0400

    Fixed bugs and added choice between LSTM vs ANN

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, request, jsonify,render_template
+from flask import Flask, request, jsonify, render_template
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import load_model
 import os
 import json
+
 app = Flask(__name__)
-# Load the trained model
-model = load_model('Lstm.h5')
+
+# Load the trained models
+model_ann = load_model('ann_model.h5')
+model_lstm = load_model('Lstm.h5')
+
 # Get the absolute path of the CSV file
 data_path = os.path.abspath('clean_data.csv')
 
 # Load the data from the CSV file
 data = pd.read_csv(data_path)
-
-
 
 # Define the target variables
 target_variables = ['CO2', 'Methane', 'Nitrous_Oxide', 'CFCs',
@@ -28,17 +30,28 @@ data_scaled = scaler.fit_transform(data[target_variables])
 
 # Define the number of time steps (lags) to consider
 time_steps = 1
+
 @app.route('/')
 def index():
     return render_template('index.html')
-# Endpoint for making predictions
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the input year from the request
-    input_year = int(request.json['year'])
-        
+    # Get the input year and model type from the request
+    input_year = int(request.form['year'])
+    model_type = request.form['model_type']
+
+    # Select the model based on the user's choice
+    if model_type == 'ann':
+        model = model_ann
+    elif model_type == 'lstm':
+        model = model_lstm
+    else:
+        return jsonify({'error': 'Invalid model type, choose between "ann" and "lstm".'})
+
+    # Rest of the code for prediction
     predicted_val = []
-    if input_year>2021:
+    if input_year > 2021:
         # Prepare the input data
         input_data = data[data['Year'] <= 2021]
         input_data_scaled = scaler.transform(input_data[target_variables])
@@ -59,11 +72,10 @@ def predict():
 
             # Get the predicted values for the current year
             predicted_values = prediction[0]
-            
+
             # Append the predicted values to the results
             predicted_val.append(predicted_values)
-            # for variable, value in zip(target_variables, prediction_val[-1]):
-            #   print(f"{variable}: {value}")
+
             # Update the input sequence for the next iteration
             input_sequences[0][:-1] = input_sequences[0][1:]  # Remove the first value
             input_sequences[0][-1] = np.array(predictions)  # Add predictions at the last index
@@ -85,22 +97,41 @@ def predict():
 
         # Inverse transform the predicted values
         predicted_val = scaler.inverse_transform(predictions)
+
     # Prepare the response
-    # Create a dictionary to store the predicted values
     prediction_dict = {}
 
-    # Populate the dictionary with variable names and predicted values
+    def convert_units(value, unit):
+        # Constants
+        total_surface_area_earth_m2 = 5.1 * 10 ** 14  # Total surface area of the Earth in m²
+        seconds_per_year = 60 * 60 * 24 * 365  # Number of seconds in a year
+        if unit == "joules per year":
+            # Conversion
+            joules_per_year = value * total_surface_area_earth_m2 * seconds_per_year
+            return joules_per_year
+        else:
+            return value  # If no conversion is required, return the original value
+
+    # Variables that require conversion to "joules per year"
+    variables_to_convert = ["CO2", "Methane", "Nitrous_Oxide", "CFCs", "Hydrochlorofluorocarbons", "Hydrofluorocarbons",
+                            "Total_Greenhouse_Gases"]
+
+    # Units for the variables
+    units = ["joules per year", "joules per year", "joules per year", "joules per year", "joules per year",
+             "joules per year", "joules per year", "ppm", "AGGI", "% change per year", "ºC", "ppm"]
+
     for variable, value in zip(target_variables, predicted_val[-1]):
-        prediction_dict[variable] = float(value)  # Convert to float
+        if variable in variables_to_convert:
+            converted_value = convert_units(value, "joules per year")
+            formatted_number = "{:.3e}".format(converted_value)
+            prediction_dict[variable] = formatted_number + " " + "joules per year"
+        else:
+            unit_index = target_variables.index(variable)
+            formatted_number = "{:.3e}".format(value)
+            prediction_dict[variable] = formatted_number + " " + units[unit_index]
 
-    # Convert the dictionary to JSON
-    prediction_json = json.dumps(prediction_dict)
-
-    # Print the JSON
-    print(prediction_json)
-    # print(jsonify(response))
     # Return the predictions as JSON response
-    return prediction_json
+    return jsonify(prediction_dict)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
